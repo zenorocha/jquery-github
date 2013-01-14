a5981c4df9da108dcd9fe2abd113ff2d43a65527
Zeno Rocha <zno.rocha@gmail.com>
2013-01-13 23:18:17 -0300

    renaming css classes

--- jquery.github.repos.js
+++ jquery.github.repos.js
@@ -56,23 +56,22 @@
             var date = new Date(repo.pushed_at);
             var pushed_at = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
             var $widget = $(' \
-              <div class="github-box repo">  \
-                  <div class="github-box-title"> \
+              <div class="github-box">  \
+                  <div class="github-box-header"> \
                       <h3> \
-                          <a class="repo" href="' + repo.url.replace('api.','').replace('repos/','') + '">' + repo.name + '</a> \
+                          <a href="' + repo.url.replace('api.','').replace('repos/','') + '">' + repo.name + '</a> \
                       </h3> \
                       <div class="github-stats"> \
-                          <a class="watchers" href="' + repo.url.replace('api.','').replace('repos/','') + '/watchers">' + repo.watchers + '</a> \
-                          <a class="forks" href="' + repo.url.replace('api.','').replace('repos/','') + '/forks">' + repo.forks + '</a> \
+                          <a class="repo-watchers" href="' + repo.url.replace('api.','').replace('repos/','') + '/watchers">' + repo.watchers + '</a> \
+                          <a class="repo-forks" href="' + repo.url.replace('api.','').replace('repos/','') + '/forks">' + repo.forks + '</a> \
                       </div> \
                   </div> \
                   <div class="github-box-content"> \
-                      <p class="description">' + repo.description + ' &mdash; <a href="' + repo.url.replace('api.','').replace('repos/','') + '#readme">Read More</a></p> \
-                      <p class="link"><a href="' + repo.homepage + '">' + repo.homepage + '</a></p> \
+                      <p>' + repo.description + ' &mdash; <a href="' + repo.url.replace('api.','').replace('repos/','') + '#readme">Read More</a></p> \
                   </div> \
                   <div class="github-box-download"> \
-                      <p class="updated">Latest commit to <strong>master</strong> on ' + pushed_at + '</p> \
-                      <a class="download" href="' + repo.url.replace('api.','').replace('repos/','') + '/zipball/master">Download as zip</a> \
+                      <p class="repo-update">Latest commit to <strong>master</strong> on ' + pushed_at + '</p> \
+                      <a class="repo-download" href="' + repo.url.replace('api.','').replace('repos/','') + '/zipball/master">Download as zip</a> \
                   </div> \
               </div> \
             ');
