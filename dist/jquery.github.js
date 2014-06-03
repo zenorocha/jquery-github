93f0fd96a952469f25d255089bd6cf59145372a9
Zeno Rocha <zeno.rocha@liferay.com>
2014-06-02 23:29:00 -0300

    Release v0.3.4

--- dist/jquery.github.js
+++ dist/jquery.github.js
@@ -1,7 +1,7 @@
 /*
- *  jQuery Github - v0.3.2
+ *  jquery-github - v0.3.4
  *  A jQuery plugin to display your Github Repositories.
- *  https://github.com/zenorocha/jquery-github/
+ *  https://github.com/zenorocha/jquery-github
  *
  *  Copyright (c) 2014
  *  MIT License
@@ -10,91 +10,84 @@
 
 function GithubRepo( repo ) {
 	this.description = repo.description;
-	this.forks = repo.forks;
+	this.forks = repo.forks_count;
 	this.name = repo.name;
 	this.open_issues = repo.open_issues;
 	this.pushed_at = repo.pushed_at;
 	this.url = repo.url;
-	this.watchers = repo.watchers;
+	this.stargazers = repo.stargazers_count;
 }
 
 // Parses HTML template
 GithubRepo.prototype.toHTML = function () {
-	var self = this;
-
-	self.pushed_at = self._parsePushedDate( self.pushed_at ),
-	self.url  = self._parseURL( self.url );
+	this.pushed_at = this._parsePushedDate( this.pushed_at ),
+	this.url  = this._parseURL( this.url );
 
 	return $(
 		"<div class='github-box'>" +
 			"<div class='github-box-header'>" +
 				"<h3>" +
-					"<a href='" + self.url + "'>" + self.name + "</a>" +
+					"<a href='" + this.url + "'>" + this.name + "</a>" +
 				"</h3>" +
 				"<div class='github-stats'>" +
-					"<a class='repo-stars' title='Stars' data-icon='7' href='" + self.url + "/watchers'>" + self.watchers + "</a>" +
-					"<a class='repo-forks' title='Forks' data-icon='f' href='" + self.url + "/network'>" + self.forks + "</a>" +
-					"<a class='repo-issues' title='Issues' data-icon='i' href='" + self.url + "/issues'>" + self.open_issues + "</a>" +
+					"<a class='repo-stars' title='Stars' data-icon='7' href='" + this.url + "/stargazers'>" + this.stargazers + "</a>" +
+					"<a class='repo-forks' title='Forks' data-icon='f' href='" + this.url + "/network'>" + this.forks + "</a>" +
+					"<a class='repo-issues' title='Issues' data-icon='i' href='" + this.url + "/issues'>" + this.open_issues + "</a>" +
 				"</div>" +
 			"</div>" +
 			"<div class='github-box-content'>" +
-				"<p>" + self.description + " &mdash; <a href='" + self.url + "#readme'>Read More</a></p>" +
+				"<p>" + this.description + " &mdash; <a href='" + this.url + "#readme'>Read More</a></p>" +
 			"</div>" +
 			"<div class='github-box-download'>" +
-				"<p class='repo-update'>Latest commit to <strong>master</strong> on " + self.pushed_at + "</p>" +
-				"<a class='repo-download' title='Download as zip' data-icon='w' href='" + self.url + "/zipball/master'></a>" +
+				"<p class='repo-update'>Latest commit to <strong>master</strong> on " + this.pushed_at + "</p>" +
+				"<a class='repo-download' title='Download as zip' data-icon='w' href='" + this.url + "/zipball/master'></a>" +
 			"</div>" +
 		"</div>");
 };
 
 // Parses pushed_at with date format
 GithubRepo.prototype._parsePushedDate = function ( pushed_at ) {
-	var self = this,
-			date = new Date( pushed_at );
+	var date = new Date( pushed_at );
 
 	return date.getDate() + "/" + ( date.getMonth() + 1 ) + "/" + date.getFullYear();
 };
 
 // Parses URL to be friendly
 GithubRepo.prototype._parseURL = function ( url ) {
-	var self = this;
-
 	return url.replace( "api.", "" ).replace( "repos/", "" );
 };
 
 // -- Github Plugin ------------------------------------------------------------
 
 function Github( element, options ) {
-	var self = this,
-			defaults = {
+	var defaults = {
 				iconStars:  true,
 				iconForks:  true,
 				iconIssues: false
 			};
 
-	self.element    = element;
-	self.$container = $( element );
-	self.repo       = self.$container.attr( "data-repo" );
+	this.element    = element;
+	this.$container = $( element );
+	this.repo       = this.$container.attr( "data-repo" );
 
-	self.options = $.extend( {}, defaults, options ) ;
+	this.options = $.extend( {}, defaults, options ) ;
 
-	self._defaults = defaults;
+	this._defaults = defaults;
 
-	self.init();
-	self.displayIcons();
+	this.init();
+	this.displayIcons();
 }
 
 // Initializer
 Github.prototype.init = function () {
-	var self   = this,
-			cached = self.getCache();
+	var cached = this.getCache();
 
 	if ( cached !== null ) {
-		self.applyTemplate( JSON.parse( cached ) );
+		this.applyTemplate( JSON.parse( cached ) );
+		return;
 	}
-	else {
-		self.requestData( self.repo );
-	}
+
+	this.requestData( this.repo );
 };
 
 // Display or hide icons
@@ -111,57 +104,49 @@ Github.prototype.displayIcons = function () {
 
 // Request repositories from Github
 Github.prototype.requestData = function ( repo ) {
-	var self = this;
+	var that = this;
 
 	$.ajax({
 		url: "https://api.github.com/repos/" + repo,
 		dataType: "jsonp",
 		success: function( results ) {
-			var result_data = results.data;
+			var result_data = results.data,
+				isFailling = results.meta.status >= 400 && result_data.message;
 
-			// Handle API failures
-			if ( results.meta.status >= 400 && result_data.message ) {
-				self.handleErrorRequest( result_data );
+			if ( isFailling ) {
+				that.handleErrorRequest( result_data );
+				return;
 			}
-			else {
-				self.handleSuccessfulRequest( result_data );
-			}
+
+			that.handleSuccessfulRequest( result_data );
 		}
 	});
 };
 
 // Handle Errors requests
 Github.prototype.handleErrorRequest = function ( result_data ) {
-	var self = this;
-
 	console.warn( result_data.message );
 	return;
 };
 
 // Handle Successful request
 Github.prototype.handleSuccessfulRequest = function ( result_data ) {
-	var self = this;
-
-	self.applyTemplate( result_data );
-	self.setCache( result_data );
+	this.applyTemplate( result_data );
+	this.setCache( result_data );
 };
 
 // Stores repostories in sessionStorage if available
 Github.prototype.setCache = function ( result_data ) {
-	var self = this;
-
 	// Cache data
 	if ( window.sessionStorage ) {
-		window.sessionStorage.setItem( "gh-repos:" + self.repo, JSON.stringify( result_data ) );
+		window.sessionStorage.setItem( "gh-repos:" + this.repo, JSON.stringify( result_data ) );
 	}
 };
 
 // Grab cached results
 Github.prototype.getCache = function() {
-	var self = this;
-
 	if ( window.sessionStorage ) {
-		return window.sessionStorage.getItem( "gh-repos:" + self.repo );
+		return window.sessionStorage.getItem( "gh-repos:" + this.repo );
 	}
 	else {
 		return false;
@@ -170,11 +155,10 @@ Github.prototype.getCache = function() {
 
 // Apply results to HTML template
 Github.prototype.applyTemplate = function ( repo ) {
-	var self  = this,
-			githubRepo = new GithubRepo( repo ),
-			$widget = githubRepo.toHTML();
+	var githubRepo = new GithubRepo( repo ),
+		$widget = githubRepo.toHTML();
 
-	$widget.appendTo( self.$container );
+	$widget.appendTo( this.$container );
 };
 
 // -- Attach plugin to jQuery's prototype --------------------------------------
